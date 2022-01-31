4a8fe3b20606fe76bc5ab8726e5121dbf8b2e31a
Nicolenco EugeniuN <eugeniu.nicolenco@tekoway.com>
2022-01-31 09:15:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gpsapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
